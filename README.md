0d773abde6b8217fdec6afc446f5eda4f6a2f503
Ryan1Up <62736925+Ryan1Up@users.noreply.github.com>
2021-09-15 09:36:10 -0700

    Server Scaffolding

---
+++ README.md
@@ -0,0 +1,4 @@
+# Smrt Grdyn
+An IoT enabled Garden Management System backed by AI
+
+This system allows you to monitor and manage your garden using a variety of sensors. The product uses AI in conjunction with the data gained from sensors to determine the best use of resources to help your plants flourish.
---
+++ webapp/smrtgrdyn/src/main/java/com/smrtgrdyn/smrtgrdyn/SmrtgrdynApplication.java
@@ -0,0 +1,16 @@
+package com.smrtgrdyn.smrtgrdyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+Main Class for Smrt Grdyn Server
+ */
+
+@SpringBootApplication
+public class SmrtgrdynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmrtgrdynApplication.class, args);
+	}
+
+}
