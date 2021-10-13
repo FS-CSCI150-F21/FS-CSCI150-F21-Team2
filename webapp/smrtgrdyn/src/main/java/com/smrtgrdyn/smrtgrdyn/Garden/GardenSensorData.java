ddc4c0896d72d663ebbd7cdc0788d2b8a88758be
Ryan1Up <62736925+Ryan1Up@users.noreply.github.com>
2021-10-12 20:55:25 -0700

    Garden data model

---
+++ webapp/smrtgrdyn/src/main/java/com/smrtgrdyn/smrtgrdyn/Garden/GardenSensorData.java
@@ -0,0 +1,94 @@
+package com.smrtgrdyn.smrtgrdyn.Garden;
+
+import java.util.UUID;
+
+public class GardenSensorData {
+/*
+    {
+        "gardenid" : "sfdds-fwgw-egv-wgeq", // generated from the server, stored on the pi
+        "wateractive" : false,
+        "temperature" : 45.3,
+        "soilmoisture" : 32.1,
+        "waterflow" : 43.1,
+        "timestamp" : "9082304823048"
+    }
+
+    smrtgrdyn.com/gardenstatus {json}
+*/
+
+    UUID gardenid;
+    boolean wateractive;
+    double temperature;
+    double soilmoisture;
+    double waterflow;
+    String timestamp;
+
+    public GardenSensorData(UUID gardenid, boolean wateractive, double temperature, double soilmoisture, double waterflow, String timestamp) {
+        this.gardenid = gardenid;
+        this.wateractive = wateractive;
+        this.temperature = temperature;
+        this.soilmoisture = soilmoisture;
+        this.waterflow = waterflow;
+        this.timestamp = timestamp;
+    }
+
+    public UUID getGardenid() {
+        return gardenid;
+    }
+
+    public void setGardenid(UUID gardenid) {
+        this.gardenid = gardenid;
+    }
+
+    public boolean isWateractive() {
+        return wateractive;
+    }
+
+    public void setWateractive(boolean wateractive) {
+        this.wateractive = wateractive;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getSoilmoisture() {
+        return soilmoisture;
+    }
+
+    public void setSoilmoisture(double soilmoisture) {
+        this.soilmoisture = soilmoisture;
+    }
+
+    public double getWaterflow() {
+        return waterflow;
+    }
+
+    public void setWaterflow(double waterflow) {
+        this.waterflow = waterflow;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "GardenSensorData{" +
+                "gardenid=" + gardenid +
+                ", wateractive=" + wateractive +
+                ", temperature=" + temperature +
+                ", soilmoisture=" + soilmoisture +
+                ", waterflow=" + waterflow +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
+}
