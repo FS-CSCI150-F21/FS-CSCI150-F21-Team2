3eb529da677c3160cb218921433fe42bffe031f8
Ryan1Up <62736925+Ryan1Up@users.noreply.github.com>
2021-10-21 20:32:39 -0700

    Code formatting and clean up

--- webapp/smrtgrdyn/src/main/java/com/smrtgrdyn/smrtgrdyn/Garden/Registration/GardenRegistrationService.java
+++ webapp/smrtgrdyn/src/main/java/com/smrtgrdyn/smrtgrdyn/Garden/Registration/GardenRegistrationService.java
@@ -31,57 +31,65 @@ public class GardenRegistrationService {
     @Autowired
     public GardenRegistrationService(GardenRegistrationRequestRepository pairingRepository,
                                      GardenConnectionInformationRepository gardenConnectionInformationRepository,
-                                     UserInformationRepository userInformationRepository){
+                                     UserInformationRepository userInformationRepository) {
         this.registrationRequestRepository = pairingRepository;
         this.gardenConnectionInformationRepository = gardenConnectionInformationRepository;
         this.userInformationRepository = userInformationRepository;
     }
 
-    public void confirmRegistration(String username, GardenRegistrationRequest request){
+    public void confirmRegistration(String username, GardenRegistrationRequest request) {
 
-        //Check registrationRequest table for request
-        //if present pull send UUID to garden and drop request
-
-        //to send UUID to garden pull connection info and open socket to pi
-        //send UUID to pi through socket
-        //This pulls a registration request from the database based on the piId
         Optional<GardenRegistrationRequest> optionalGardenRegistrationRequest =
                 registrationRequestRepository.findById(request.getPiId());
 
-        //Checks the request against the username
-        if(optionalGardenRegistrationRequest.isPresent()){
-            if(optionalGardenRegistrationRequest.get().getUsername().equals(username)){
+        if (optionalGardenRegistrationRequest.isPresent()) {
+            if (optionalGardenRegistrationRequest.get().getUsername().equals(username)) {
 
-             //   sendUUIDToPi(optionalGardenRegistrationRequest.get().getGardenId());
+                /*
+                *
+                *  sendUUID to pi will currently NOT work as the server only tests locally
+                *
+                *
+                * */
+                //   sendUUIDToPi(optionalGardenRegistrationRequest.get().getGardenId());
 
                 registerGardenWithUser(optionalGardenRegistrationRequest.get().getUsername(),
-                          optionalGardenRegistrationRequest.get().getGardenId());
+                        optionalGardenRegistrationRequest.get().getGardenId());
 
-                //then drop the registration request from the db
                 registrationRequestRepository.deleteById(optionalGardenRegistrationRequest.get().getPiId());
-                System.out.println("Registration confirmed");
 
-            }else{
+            } else {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid User");
             }
-
-
-        }else{
+        } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Registration Request Not Found");
-
         }
     }
 
-    private void sendUUIDToPi(UUID gardenId){
+    private void registerGardenWithUser(String username, UUID gardenId) {
+        Optional<User> userOptional = userInformationRepository.findById(username);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            //   user.addGarden(gardenId);
+            user.getRegisteredGardens().add(gardenId);
+            userInformationRepository.save(user);
+
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not Registered");
+        }
+    }
+
+    private void sendUUIDToPi(UUID gardenId) {
 
         Optional<GardenConnectionInformation> optionalGardenConnectionInformation =
                 gardenConnectionInformationRepository.findById(gardenId);
 
-        if(optionalGardenConnectionInformation.isPresent()){
+        if (optionalGardenConnectionInformation.isPresent()) {
             try {
                 Socket socket = new Socket(optionalGardenConnectionInformation.get().getHostName(),
                         optionalGardenConnectionInformation.get().getPortNumber());
-                if(socket.isConnected()){
+                if (socket.isConnected()) {
                     //Setup Output Stream
                     OutputStream outputStream = socket.getOutputStream();
                     DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
@@ -100,51 +108,31 @@ public class GardenRegistrationService {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not Connect to Pi", e);
             }
         }
-
     }
-    public void requestRegistration(HttpServletRequest servletRequest, GardenRegistrationRequest registrationRequest)  {
 
-        //1. Get Ip/Host Name of the request
-        //2. Get the Port Number
+    public void requestRegistration(HttpServletRequest servletRequest, GardenRegistrationRequest registrationRequest) {
+
         String host = servletRequest.getHeader(HttpHeaders.HOST);
         Integer port = servletRequest.getServerPort();
-
-        //Generate UUID for Garden
         UUID gardenId = UUID.randomUUID();
-        // Get Username for Garden information entry
         String username = registrationRequest.getUsername();
 
         this.gardenConnectionInformation = new GardenConnectionInformation(gardenId, username, host, port);
+
         this.gardenRegistrationRequest = registrationRequest;
         this.gardenRegistrationRequest.setGardenId(gardenId);
-        //Registration Request should contain all information
+
+
         registrationRequestRepository.save(this.gardenRegistrationRequest);
-        gardenConnectionInformationRepository.save(gardenConnectionInformation);
+        gardenConnectionInformationRepository.save(this.gardenConnectionInformation);
 
-        //Start timing thread to drop entry once time limit is up
-        // Look up a thread or runnable statement
-        GardenRegistrationServiceThread thread =
-                new GardenRegistrationServiceThread(
-                        gardenRegistrationRequest.getPiId(), gardenId);
-        thread.start();
+
+        new GardenRegistrationServiceThread(gardenRegistrationRequest.getPiId(), gardenId).start();
 
     }
 
-    private void registerGardenWithUser(String username, UUID gardenId){
-        Optional<User> userOptional = userInformationRepository.findById(username);
 
-        if(userOptional.isPresent()){
-            User user = userOptional.get();
-            //   user.addGarden(gardenId);
-            user.getRegisteredGardens().add(gardenId);
-            userInformationRepository.save(user);
-
-        }else{
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not Registered");
-        }
-    }
-
-    private class GardenRegistrationServiceThread extends Thread{
+    private class GardenRegistrationServiceThread extends Thread {
 
         private String piId;
         UUID gardenId;
@@ -156,20 +144,19 @@ public class GardenRegistrationService {
 
         @Override
         public void run() {
-            try{
-
+            try {
+                //Sleep time denotes how long the registration request can stay open
                 Thread.sleep(300000);
 
                 Optional<GardenRegistrationRequest> registrationRequestOptional =
                         registrationRequestRepository.findById(this.piId);
 
-                //If it is present, that means registration failed, drop entries from both tables
-                if (registrationRequestOptional.isPresent()){
+                if (registrationRequestOptional.isPresent()) {
                     gardenConnectionInformationRepository.deleteById(this.gardenId);
                     registrationRequestRepository.deleteById(this.piId);
                 }
 
-            }catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
