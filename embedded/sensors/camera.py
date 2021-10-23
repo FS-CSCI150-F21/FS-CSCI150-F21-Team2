10e3634dd01934c5aa1844c7101df19759e0880f
jwhitlow45 <jwhitlow45@gmail.com>
2021-10-23 02:51:38 +0100

    feat:add function to capture image from rpi camera

---
+++ embedded/sensors/camera.py
@@ -0,0 +1,27 @@
+from picamera import PiCamera
+from datetime import datetime
+import os
+
+def capture_image():
+    """
+    Captures an image and returns the path to the captured image
+    args:
+        None
+    returns:
+        str: path to image
+    """
+
+    _image_folder = str(os.getcwd() + '/.images/')
+
+    # prevent access attempt of non-existent folder
+    if not os.path.isdir(_image_folder):
+        os.mkdir(_image_folder)
+
+    # provide unique name for each image
+    timestamp = datetime.utcnow().strftime("%Y_%m_%d_%H%M%S")
+    image_path = str(_image_folder + timestamp + '.jpeg')
+
+    camera = PiCamera()
+    camera.capture(image_path)
+
+    return image_path
