7ea75379ea26c71670bf529166ddc65437138418
nwanderson009 <82849228+nwanderson009@users.noreply.github.com>
2021-11-02 15:46:26 -0700

    Create smrt_gyrdn.sql

---
+++ database/smrt_gyrdn.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `connection_information`
+--
+
+DROP TABLE IF EXISTS `connection_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `connection_information` (
+  `username` varchar(45) NOT NULL,
+  `garden_id` varchar(45) NOT NULL,
+  `host` varchar(45) DEFAULT NULL,
+  `port` int DEFAULT NULL,
+  PRIMARY KEY (`garden_id`),
+  KEY `username` (`username`),
+  CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `user_information` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `garden_images`
+--
+
+DROP TABLE IF EXISTS `garden_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `garden_images` (
+  `garden_id` varchar(45) NOT NULL,
+  `timestamp` timestamp NOT NULL,
+  `image_path` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`garden_id`,`timestamp`),
+  CONSTRAINT `garden_id_images` FOREIGN KEY (`garden_id`) REFERENCES `connection_information` (`garden_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `registration_request`
+--
+
+DROP TABLE IF EXISTS `registration_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registration_request` (
+  `registration_id` varchar(45) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `garden_id` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`registration_id`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `username_registration_request` FOREIGN KEY (`username`) REFERENCES `user_information` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sensor_data`
+--
+
+DROP TABLE IF EXISTS `sensor_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sensor_data` (
+  `garden_id` varchar(45) NOT NULL,
+  `timestamp` timestamp NOT NULL,
+  `water_flow` int DEFAULT NULL,
+  `pH_balance` int DEFAULT NULL,
+  `soil_moisture` int DEFAULT NULL,
+  `temperature` int DEFAULT NULL,
+  `water_active` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`garden_id`,`timestamp`),
+  CONSTRAINT `garden_id` FOREIGN KEY (`garden_id`) REFERENCES `connection_information` (`garden_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_information`
+--
+
+DROP TABLE IF EXISTS `user_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_information` (
+  `username` varchar(45) NOT NULL,
+  `salt` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-23 16:24:33
