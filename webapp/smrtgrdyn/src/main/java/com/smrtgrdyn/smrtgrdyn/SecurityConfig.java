a0628c1d2fad2200ef135223268a01e2d0f5a0cf
Ryan1Up <62736925+Ryan1Up@users.noreply.github.com>
2021-11-14 15:34:23 -0800

    Bug Fix: NotificationUtil needed to be instantiaed as a Bean via @Service annotation

--- webapp/smrtgrdyn/src/main/java/com/smrtgrdyn/smrtgrdyn/SecurityConfig.java
+++ webapp/smrtgrdyn/src/main/java/com/smrtgrdyn/smrtgrdyn/SecurityConfig.java
@@ -9,7 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-        http.csrf().disable();
+//        http.authorizeRequests().anyRequest().permitAll();
+//        http.csrf().disable();
     }
 }
