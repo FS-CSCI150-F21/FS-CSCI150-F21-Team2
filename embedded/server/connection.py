459850375e23c6ce5936eb0663c325876a6226e7
jwhitlow45 <jwhitlow45@gmail.com>
2021-11-18 22:43:19 +0000

    refactor:change structure to store variables in other files to clean up implementation

--- embedded/server/connection.py
+++ embedded/server/connection.py
@@ -7,20 +7,13 @@ import os
 from utils.logging import log
 from utils.interaction import get_yn_response
 import server.json_bodies as body
+from endpoints import Endpoint
 
 # Global variables for file
 __server_folder = os.path.normpath(os.path.dirname(__file__) + os.sep)
 __registration_file = str(__server_folder + '/.registration')
 
-# Base URL
-__base_url : any
-with open(str(__server_folder + '/.url'), 'r') as FILE:
-    __base_url = FILE.readline()
-# API Endpoints
-__garden_registration_endpoint = str(__base_url + 'api/v1/garden_registration/pi')
-__garden_image_endpoint = str(__base_url + 'api/v1/images')
-__garden_notification_endpoint = str(__base_url + 'api/v1/notifications')
-
+# Registration prefixes
 __username_pref = 'username='
 __gardenID_pref = 'gardenID='
 
@@ -106,8 +99,9 @@ def register(timeout: int = 300000):
     pairing_code = generate_pairing_code()
     log(f'Your pairing code is [{pairing_code}], you have {timeout/60} minutes before it expires!')
 
+    # Generate POST request body and send post request
     registration_body = body.PiRegistration(username, pairing_code).__dict__
-    uuid = requests.post(__garden_registration_endpoint, json=registration_body)
+    uuid = requests.post(Endpoint.REGISTRATION_ENDPOINT, json=registration_body)
     gardenID = uuid.content.decode('utf-8')
     
     # Write new credential to registration file
