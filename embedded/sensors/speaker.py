52df6a1ca18e73b4009a8b6160f8ef3b2919d268
Jose <65420011+itspalomo@users.noreply.github.com>
2021-11-17 18:31:58 -0800

    refactoring some code structure

--- embedded/sensors/speaker.py
+++ embedded/sensors/speaker.py
@@ -3,35 +3,33 @@ import board
 import digitalio
 from threading import Thread, Lock
 
-
 mutex = Lock()
 
 Speaker = digitalio.DigitalInOut(board.D2)  # map the light to GPIO 2 on the rpi (pin 13)
 Speaker.direction = digitalio.Direction.OUTPUT  # set gpio as a digital output
 
-mutex.acquire()
 
-
-def warning1(pattern):
-
+def play_sound_pattern(list_pattern, lock):
+    lock.acquire()
     timeout = time.time() + 60 * 5  # 5 minutes from now
 
-    count = 0
+    pattern_index = 0
 
     while True:  # run through
-        if count > len(pattern):
-            count = 0
+        if pattern_index > len(list_pattern):
+            pattern_index = 0
 
         Speaker.value = True
-        time.sleep(pattern[count])
+        time.sleep(list_pattern[pattern_index])
         Speaker.value = False
-        count += 1
-        time.sleep(pattern[count])
-        count += 1
+        pattern_index += 1
+        time.sleep(list_pattern[pattern_index])
+        pattern_index += 1
         if time.time() > timeout:
             break
 
+    lock.release()
 
-mutex.release()
 
-
+def speaker_status():
+    pass
