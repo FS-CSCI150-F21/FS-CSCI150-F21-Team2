1bddacda37a4499c0b71e37129fbb713751f16c3
Jose <65420011+itspalomo@users.noreply.github.com>
2021-11-23 10:12:00 -0800

    analog device class restructuring

--- embedded/sensors/analogdevice.py
+++ embedded/sensors/analogdevice.py
@@ -1,9 +1,23 @@
-import sensors.digitaldevice
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+from threading import Thread, Lock
 
-analog_occupied = None
+
+class AnalogDevice():
+    __i2c = busio.I2C(board.SCL, board.SDA)
+    __ads = ADS.ADS1115(__i2c)
+    __occupied = None
+
+    def __init__(self, channel, pin):
+        if AnalogDevice.__occupied is not None and AnalogDevice.__occupied[pin]:
+            raise ValueError("Pin is already assigned")
+
+        self.channel = AnalogIn(AnalogDevice.__ads, ADS.P0)
+
+    def read_soil_moisture(self):
+        return self.channel.value
 
 
-class AnalogToDigDevice(sensors.digitaldevice.DigitalDevice):
-    def __init__(self, device_id, direction, pin, adc_pin):
-        super().__init__(device_id, direction, pin)
-        self.adc_pin = adc_pin;
+mutex.release()
