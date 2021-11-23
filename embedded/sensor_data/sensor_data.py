2f22f7aa317c8857e00b9f9e63a6468a8e177b05
Jose <65420011+itspalomo@users.noreply.github.com>
2021-11-23 14:15:49 -0800

    building test file for the sensors, sensor data gathering

--- embedded/sensor_data/sensor_data.py
+++ embedded/sensor_data/sensor_data.py
@@ -1,3 +1,13 @@
-def test():
-    print('test')
+import sensors.analogdevice
+import sensors.digitaldevice
+import sensors.speaker
+import sensors.solenoid
+import sensors.soilmoisture
+import sensors.light12v
+import sensors.flow_meter
+import sensors.temperature
+
+
+# global objects
+if __name__ == "__main__":
     pass
