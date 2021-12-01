b17051bab499595532e7f729e6d60cdf492e2bf2
jwhitlow45 <jwhitlow45@gmail.com>
2021-12-01 15:36:25 +0000

    feat:finish implementing methods to modify status of automated systems

--- embedded/utils/status.py
+++ embedded/utils/status.py
@@ -11,7 +11,7 @@ def __read_status_file() -> Tuple[bool, bool]:
     Returns status of automated systems in (animal status, watering status) format
 
     Args:
-        None:
+        None
     Returns
         Tuple(bool,bool): Tuple containing status of automated systems
     """
@@ -26,14 +26,62 @@ def __read_status_file() -> Tuple[bool, bool]:
 
     return (animal_status, watering_status)
 
+def __write_status_file(status: Tuple[bool, bool]):
+    """
+    Writes status of automated systems to .status file
 
-def get_automatic_watering_status():
-    with open(__status_file, 'r') as FILE:
-        FILE.r
+    Args:
+        Tuple(bool,bool): State to set status of automated systems to
+    Returns:
+        None
+    """
 
-def set_automatic_watering_status():
+    animal_status_line = __animal_prefix + 'enabled' if status[0] else 'disabled'
+    watering_status_line = __watering_prefix + 'enabled' if status[1] else 'disabled'
+    
+    with open(__status_file, 'w') as FILE:
+        FILE.writelines([animal_status_line, watering_status_line])
 
-def get_animal_detection_status():
 
-def set_automatic_watering_status():
+def get_animal_detection_status() -> bool:
+    """
+    Returns status of animal detection system
+    Args:
+        None
+    Returns:
+        bool: animal detection status
+    """
+    return __read_status_file()[0]
+
+def get_automatic_watering_status() -> bool:
+    """
+    Returns status of automatic watering system
+    Args:
+        None
+    Returns:
+        bool: automatic watering status
+    """
+    return __read_status_file()[1]
+
+def set_animal_detection_status(animal_status: bool):
+    """
+    Set status of animal detection (enabled/disabled)
+    Args:
+        bool: status to set animal detection
+    Returns:
+        None
+    """
+    watering_status = get_automatic_watering_status()
+    __write_status_file((animal_status, watering_status))
+
+def set_automatic_watering_status(watering_status: bool):
+    """
+    Set status of automatic watering (enabled/disabled)
+    Args:
+        bool: status to set automatic watering
+    Returns:
+        None
+    """
+    animal_status = get_animal_detection_status()
+    __write_status_file((animal_status, watering_status))
 
