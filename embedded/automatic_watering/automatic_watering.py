085a7c6adba166f08c9eae3e357b30d8f558e049
Jose <65420011+itspalomo@users.noreply.github.com>
2021-12-01 01:33:33 -0800

    small edits to automatic watering

--- embedded/automatic_watering/automatic_watering.py
+++ embedded/automatic_watering/automatic_watering.py
@@ -9,32 +9,33 @@ import sensor_data.gather_sensor_data as get_data
 
 # global variables needed for the process
 
-__AirValue = 520;
-__WaterValue = 260;
-__intervals = (__AirValue - __WaterValue) / 3;
+__DryValue = 520;
+__WetValue = 260;
+__Threshold = (__DryValue - __WetValue) / 3
 
 
 # The moisture thresholds are split into 3 levels of moisture Very wet, wet, and dry
 def automatic_watering():
     garden_data = get_data.gather_all_sensor_data()
     soilMoistureValue = garden_data[4]
-    if __WaterValue < soilMoistureValue < (__WaterValue + __intervals):
+    if __WetValue < soilMoistureValue < (__WetValue + __Threshold):
         garden_data[3].pin_off()
         print("Very Wet")
         leak_detection(garden_data[3], garden_data[2])
-    elif (__WaterValue + __intervals) < soilMoistureValue < (__AirValue - __intervals):
+    elif (__WetValue + __Threshold) < soilMoistureValue < (__DryValue - __Threshold):
         garden_data[3].pin_off()
         print("Wet")
-    elif __AirValue > soilMoistureValue > (__AirValue - __intervals):
+    elif __DryValue > soilMoistureValue > (__DryValue - __Threshold):
         garden_data[3].pin_on()
         print("Dry")
-    time.sleep(500)
+    time.sleep(5000)
 
 
 def leak_detection(solenoid: Sol, flow_rate):
     if solenoid().get_solenoid_status() == False and flow_rate() > 0:
         print("Potential water leak detected")
         warn()
+
 
 def warn(case):
     pass
