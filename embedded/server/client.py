4a59ba1513d8e02b490ef7eba4b13a2acd968e58
jwhitlow45 <jwhitlow45@gmail.com>
2021-12-01 19:14:40 +0000

    feat:add method to parse post body

--- embedded/server/client.py
+++ embedded/server/client.py
@@ -1,9 +1,17 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 
+import server.connection as sc
+import server.json_bodies as body
+
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _set_malformed_response(self, msg: Exception):
+        self.send_response(400, message=str(msg))
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
@@ -18,8 +26,33 @@ class S(BaseHTTPRequestHandler):
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        try:
+            CurServerRequest = self.get_body(post_data)
+            self._set_response()
+            self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+            
+        except Exception as e:
+            self._set_malformed_response(e)
+            logging.info('POST request malformed.')
+        
+    def get_request_body(self, response) -> body.ServerRequest:
+        # parse POST request for body
+        post_body = response.decode()
+        post_body = post_body.split('\n')
+        # remove unneeded lines
+        post_body = [line.strip() for line in post_body if line.strip() != '' and line[0] != '-']
+        # get keys from even lines by finding key after = sign
+        keys = [post_body[i][post_body[i].find('=')+2:-1] for i in range(0, len(post_body), 2)]
+        # get values from odd lines
+        values = [post_body[i] for i in range(1, len(post_body), 2)]
+        
+        for key, value in zip(keys, values):
+            post_body[key] = value
+
+        # convert to object and return
+        return body.ServerRequest(**post_body)
+
+
 
         
 
