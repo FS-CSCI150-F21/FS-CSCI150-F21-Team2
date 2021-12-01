9174b85e5301c3ddd941366726d82c49eeab0b45
jwhitlow45 <jwhitlow45@gmail.com>
2021-12-01 19:44:56 +0000

    feat:add server thread to main

--- embedded/main.py
+++ embedded/main.py
@@ -6,16 +6,22 @@ import interactive_terminal.terminal as term
 from threading import Thread
 from utils.logging import log
 import utils.status as us
+import server.client as client
 
 def main():
 	os.system('clear')
 	
 	animal_det_thread = Thread(target=animal_detector.detection_loop)
+	server_thread = Thread(target=client.run)
+
 	if us.get_animal_detection_status():
 		animal_det_thread.start()
+	server_thread.start()
 		
 	sleep(0.5)
 	term.terminal_loop()
+
+
 
 if __name__ == '__main__':
 	main()
