8b3e8aa23c0aea342d5330f264ee9a21ba9b8d04
jwhitlow45 <jwhitlow45@gmail.com>
2021-12-02 20:41:33 +0000

    feat:add method to print out iso datetime format with T instead of space

--- embedded/utils/logging.py
+++ embedded/utils/logging.py
@@ -3,3 +3,9 @@ from datetime import datetime
 def log(message: str, end: str = '\n'):
     now = datetime.utcnow()
     print(f'[{now}]:{message}', end=end)
+
+def get_cur_iso_timestamp_t():
+    # Get ISO timestamp with T instead of space
+    timestamp = str(datetime.utcnow())
+    timestamp_t = timestamp[:10] + 'T' + timestamp[11:]
+    return timestamp_t
